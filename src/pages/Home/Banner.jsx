59a4fb9e1c7c2c9e9478f805811dd266a8af2a8d
NikkuYadav <ry027674@gmail.com>
2025-03-31 11:56:44 +0530

    whatsapp phone

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -4,7 +4,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { assets } from "../../assets/assets";
-import { FaArrowCircleRight } from "react-icons/fa";
 
 const Banner = () => {
     const images = [
